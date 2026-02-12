ecd036a88c53b43ab9a39de6dc4d6633e7e57b08
kallebesiqueira-dev <kallebesiqueira@gmail.com>
2026-02-12 11:09:26 +0100

    Cleanup unused backend files and update security deps

--- backend/messages.js
+++
@@ -1,7 +0,0 @@
-
-const messageController = require("./controllers/messageController");
-
-module.exports = {
-  listMessages: messageController.listMessages,
-  addMessage: messageController.addMessage,
-};
